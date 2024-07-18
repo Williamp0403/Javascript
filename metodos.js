face36d66be614c3fffa3312ff585b56d4aec4a6
William Pinto <williamp0403@gmail.com>
2024-07-18 18:49:23 -0400

    Agregando objeto Math, historia de cofla 4 y nuevos metodos

--- metodos.js
+++ metodos.js
@@ -1,8 +1,63 @@
-// Strings
+//// Strings ////
 
-// Split
+// Concat: Junta dos o mas cadenas y retorna una nueva
 
-// Convierte un string en un arreglo, separandolo por el parametro dado.
+var cadena = "cadena de prueba";
+var cadena2 = cadena.concat(" cadena 2")
+console.log(cadena2);
+
+// Startswith: Si una cadena comienza con los caracteres de otra cadena, devuelve true, sino, devuelve false.
+
+var cadena = "cadena de prueba";
+var cadena2 = "cadena";
+var resultado = cadena.startsWith(cadena2);
+console.log(resultado);
+
+// Endswith: Si una cadena termina con los caracteres de otra cadena, devuelve true, sino, devuelve false.
+
+var cadena = "cadena de prueba";
+var cadena2 = "cadena";
+var resultado = cadena.endsWith(cadena2);
+console.log(resultado);
+
+// Includes: Si una cadena puede encontrarse dentro de otra cadena, devuelve true, sino, devuelve false. 
+
+var cadena = "cadena de prueba";
+var cadena2 = "de";
+var resultado = cadena.includes(cadena2);
+console.log(resultado);
+
+// IndexOf: Devuelve el indice del primer caracter de la cadena, si no existe, devuelve -1. 
+
+var cadena = "cadena de prueba";
+var resultado = cadena.indexOf("prueba");
+console.log(resultado);
+
+//LastIndefOf: Devuelve el ultimo indice del primer caracter de la cadena, si no existe, devuelve -1.
+
+var cadena = "cadena de prueba prueba";
+var resultado = cadena.lastIndexOf("prueba");
+console.log(resultado);
+
+// PadStart [propuesta de Estandar]: Rellenar cadena al principio con los caracteres deseados. 
+
+var cadena = "abc"
+var resultado = cadena.padStart(10, "12");
+console.log(resultado);
+
+// PadEnd [popuesta de ECMA]: Rellenar cadena al final con los caracteres deseados.
+
+var cadena = "abc";
+var resultado = cadena.padEnd(10, "12");
+console.log(resultado);
+
+// Repeat: Devuelve la misma cadena pero repetida la cantidad que le indiquemos. 
+
+var cadena = "abc "
+var resultado = cadena.repeat(4);
+console.log(resultado);
+
+// Split: Convierte un string en un arreglo, separandolo por el parametro dado.
 
 const str = "The quick brown fox jumps over the lazy dog.";
 const words = str.split(" ");
@@ -11,32 +66,62 @@ console.log(words);
 var prueba2 = "Hola, Mundo";
 console.log(prueba2.split(",")[1]);
 
-// Uppercase
+// Substring: Nos retorna un pedazo de la cena que seleccionamos.
 
-// O método toUpperCase() retorna el valor de un string convertido en mayusculas.
+var cadena = "abcdefghij";
+var resultado = cadena.substring(1, 4);
+console.log(resultado);
+
+// ToUppercase: El método toUpperCase() retorna el valor de un string convertido en mayusculas.
 
 var nombre = "Juan";
 console.log(nombre.toUpperCase());
 
-// Lowercase
-
-// O método toLowerCase() retorna el valor de un string convertido en minusculas.
+// ToLowercase: El método toLowerCase() retorna el valor de un string convertido en minusculas.
 
 var nombre = "Juan";
 console.log(nombre.toLowerCase());
 
-// Replace
+// Tostring:  Convierte una cadena a String
 
-// O método replace() retorna uma nova string com algum ou todas as combinações de um padrão substituído por um substituto. O padrão pode ser uma string ou uma RegExp, e o substituto pode ser uma string ou uma função a ser chamada para cada combinação.
+var cadena = ["pedro", "maria"];
+var resultado = cadena.toString();
+console.log(typeof resultado);
+console.log(resultado[1]);
+
+// Trim: Elimina los espacios en blanco al principio y al final de una cadena.
+
+var cadena = "  hola   ";
+var resultado = cadena.trim();
+console.log(cadena);
+console.log(resultado);
+
+// TrimEnd: Elimina los espacios en blanco al final de una cadena. 
+
+var cadena = "  hola   ";
+var resultado = cadena.trimEnd();
+console.log(cadena);
+console.log(resultado);
+
+// TrimStart: Elimina los espacios en blanco al comienzo de una cadena.
+
+var cadena = "  hola   ";
+var resultado = cadena.trimStart();
+console.log(cadena);
+console.log(resultado);
+
+// ValueOf: Retorna el valor primitivo de un objeto string
+
+// Replace
+// El método replace() retorna uma nova string com algum ou todas as combinações de um padrão substituído por um substituto. O padrão pode ser uma string ou uma RegExp, e o substituto pode ser uma string ou uma função a ser chamada para cada combinação.
 
 var str2 = "Mr Blue has a blue house and a blue car";
 var res = str2.replace(/house/g, "casa");
 console.log(res);
 
-// Numbers
+//// Numbers ////
 
 // parseInt
-
 // permite convertir un string a un número entero.
 
 var numero = "10";
@@ -48,7 +133,6 @@ console.log(numero_decimal);
 console.log(parseInt(numero_decimal));
 
 // parseFloat
-
 // permite convertir un string a un número decimal.
 
 var numero = "10.5";
@@ -56,7 +140,6 @@ console.log(numero);
 console.log(parseFloat(numero));
 
 // Math.ceil
-
 // retorna el entero más cercano mayor o igual a un número dado.
 
 var numero = 10.5;
@@ -64,46 +147,85 @@ console.log(numero);
 console.log(Math.ceil(numero));
 
 // Math.floor
-
 // retorna el entero más cercano menor o igual a un número dado.
 
 var numero = 10.5;
 console.log(numero);
 console.log(Math.floor(numero));
 
-// Arrays
+//// Arrays ////
 
-// Join
+// Pop: Elimina el ultimo elemento de un array y lo devuelve.
 
-// Permite unir todos los elementos de un array en un string.
+var array = ["pedro", "juan", "jose"];
+console.log("Original:", array);
+var resultado = array.pop();
+console.log("Removido:", resultado);
+console.log("Resultado: ", array);
+
+// Shift: Elimina el primer elemento de un array y lo devuelve.
+
+var array = ["pedro", "juan", "jose"];
+console.log("Original:", array);
+var resultado = array.shift();
+console.log("Removido:", resultado);
+console.log("Resultado: ", array);
+
+// Push: Agrega un elemento al array al final de la lista.
+
+var array = ["pedro", "juan", "jose"];
+console.log("Original:", array);
+var resultado = array.push("William", "Pinto");
+console.log("Resultado: ", array);
+
+// Reverse: Invierte el orden de los elementos de un array.
+
+var array = [1, 2, 3, 4, 5];
+console.log("Original:", array)
+var resultado = array.reverse();
+console.log("Resultado", array);
+
+// Unshift: Agrega uno o mas elementos al inicio del array, y devuleve la nueva longitud del array.
+
+var array = ["pedro", "juan", "jose"];
+console.log("Original:", array);
+var resultado = array.unshift("William", "Pinto");
+console.log("Resultado: ", array);
+
+// Sort: Ordena los elementos de un arreglo localmente y devuelve el arreglo ordenado.
+
+var numeros = [2, 5, 100, 14, 10];
+var cadena = ["Manzana", "Pera", "Coco", "Guayaba"]
+cadena.sort();
+numeros.sort((a,b)=> a - b);
+console.log(cadena);
+console.log(numeros);
+
+// Splice: Cambia el contenido de un array eliminando existentes y/o agregando nuevos elementos. ejemplo.splice(comienza, eliminar, elemento)
+
+var cadena = ["Manzana", "Pera", "Coco", "Guayaba"]
+console.log("Original:", cadena);
+cadena.splice(1, 0, "Zandia", "Cambur");
+console.log("Modificado:", cadena);
+
+// Join: Permite unir todos los elementos de un array en un string.
 
 var frutas = ["Banana", "Orange", "Apple", "Mango"];
-console.log(frutas.join(" - "));
+var resultado = frutas.join(" - ")
+console.log(resultado);
 
 var word = "Hola Mundo";
 var word_array = word.split(" ");
 var array_reverse = word_array.reverse();
 console.log(array_reverse.join(" , "));
 
-// Reverse
+// Slice: Devuelve una parte del array dentro de un nuevo array empezando por inicio hasta fin (fin no incluido).
 
-// Invierte el orden de los elementos de un array.
+var frutas = ["Banana", "Orange", "Apple", "Mango", "Pera", "Cambur"]
+var resultado = frutas.slice(1, 3)
+console.log(resultado);
 
-var frutas = ["Banana", "Orange", "Apple", "Mango"];
-console.log(frutas.reverse());
-
-// Sort
-
-// Ordena los elementos de un array.
-
-var frutas = ["Banana", "Orange", "Apple", "Mango"];
-console.log(frutas.sort());
-var numeros = [88, 1, 7, 5, 10, 3];
-console.log(numeros.sort((a, b) => a - b));
-
-// Length
-
-// Retorna la longitud de un array.
+// Length: Retorna la longitud de un array.
 
 var frutas = ["Banana", "Orange", "Apple", "Mango"];
 console.log(frutas.length);
@@ -115,7 +237,7 @@ console.log(frutas.length);
 var nombres = [
   "Juan",
   "Pedro",
-  "Luis",
+  "Ana",
   "Carlos",
   "Victor",
   "Eva",
@@ -125,6 +247,21 @@ var nombres = [
 
 console.log(nombres.indexOf("Ana"));
 
+//LastIndefOf: Devuelve el ultimo indice del primer caracter de la cadena, si no existe, devuelve -1.
+
+var nombres = [
+  "Juan",
+  "Pedro",
+  "Ana",
+  "Carlos",
+  "Victor",
+  "Eva",
+  "Ana",
+  "Maria",
+];
+
+console.log(nombres.lastIndexOf("Ana"));
+
 // Includes
 
 // Determina si un array incluye un determinado elemento, devuelve true o false según corresponda.
@@ -132,7 +269,23 @@ console.log(nombres.indexOf("Ana"));
 var cedulas = [123456, 789456, 123456, 456789];
 console.log(cedulas.includes(12345689));
 
-// Objects
+// Tostring:  Convierte una cadena a String
+
+var cadena = ["pedro", "maria"];
+var resultado = cadena.toString();
+console.log(typeof resultado);
+console.log(resultado[1]);
+
+// Filter: Crea un nuevo array con todos los elementos que cumplan la condicion.
+
+var nombres = ["Juan", "Pedro", "Ana", "Carlos","Victor", "Eva", "Ana", "Maria"];
+
+resultado = nombres.filter(name=> name.length > 4)
+console.log(resultado)
+
+// ForEach: Ejecuta la funcion indicada una vez para cada elemento
+
+//// Objects
 
 // Includes
 
---
+++ objeto_math.js
@@ -0,0 +1,83 @@
+//// Metodos ////
+
+// sqrt(): Devuelve la raiz cuadrada positiva de un numero/
+
+var raizCuadrada = Math.sqrt(4);
+console.log(raizCuadrada);
+
+// cbrt(): Devuelve la raiz cubica de un numero.
+
+var raizCubica = Math.cbrt(4);
+console.log(raizCubica);
+
+// max(): Devuelve el mayor de cero o mas numeros.
+
+var numeros = Math.max(1, 2, 39, 21, 544, 221, 31, 3, 44,)
+console.log(numeros);
+
+// min(): Devuelve el mas pequeño de cero o mas numeros.
+
+var numeros = Math.min(1, 2, 39, 21, 544, 221, 31, 3, 44,)
+console.log(numeros);
+
+// random(): Devuelve un numero pseudo-aleatorio entre o y 1.
+
+var random = Math.random()
+console.log(random);
+
+// round(): Devuelve el valor de un numero redondeado al numero entero mas cercano.
+
+var num = Math.round(1.5)
+console.log(num);
+
+// fround(): Devuelve la representacion flotante de precision simple mas cercana de un numero.
+
+console.log(Math.fround(2.314141343143143))
+
+// floor(): Devuelve el mayor entero menor que o igual a un numero.
+
+var numero = Math.floor(2.3);
+console.log(numero);
+
+// ceil(): Retorna el entero más cercano mayor o igual a un número dado.
+
+var numero = Math.ceil(2.3);
+console.log(numero);
+
+// trunc(): Devuelve la parte entera del numero x, la eliminacion de los digitos fraccionarios.
+
+console.log(Math.trunc(10.99))
+
+//// Propiedades ////
+
+// PI: Radio de la circunferencia de un circulo respectp a su diametro.
+
+console.log(Math.PI);
+
+// SQRT1_2: Raiz cuadrada de 1/2; equivalente 1, sobre la raiz.
+
+console.log(Math.SQRT1_2);
+
+// SQRT2: Raiz cuadrada de 2, aproximadamente 1.414.
+
+console.log(Math.SQRT2);
+
+// E: Costante de Euler, la base de los logaritmos naturales.
+
+console.log(Math.E);
+
+// LN2: Logaritmo natural de 2, aproximadamente 0.693.
+
+console.log(Math.LN2);
+
+// LN10: Logaritmo natural de 10, aproximadamente 2.303.
+
+console.log(Math.LN10);
+
+// LOG2E: Logaritmo de E con base 2, aproximadamente 1,443.
+
+console.log(Math.LOG2E);
+
+// LOG10E: Logaritmo de E con base 10, aproximadambente 0.434
+
+console.log(Math.LOG10E);
